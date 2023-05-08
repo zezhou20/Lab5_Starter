d1fed63422f0b55370e38bc2c85b22cdd78bdbe2
Zejun Zhou <zezhou@ucsd.edu>
2023-05-07 22:34:38 -0700

    1

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,7 +3,46 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
-}
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.getElementById('voice-select');
+  const talkButton = document.querySelector('button');
+  const textarea = document.getElementById('text-to-speak');
+  const imgElement = document.querySelector('img');
+  
+  const closedMouth = 'assets/images/smiling.png';
+  const openMouth = 'assets/images/talking.png';
 
-// the end
+  function populateVoiceList() {
+    const voices = synth.getVoices();
+    voiceSelect.innerHTML = '';
+
+    for (const voice of voices) {
+      const option = document.createElement('option');
+      option.textContent = voice.name + ' (' + voice.lang + ')';
+      option.setAttribute('data-lang', voice.lang);
+      option.setAttribute('data-name', voice.name);
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  if (typeof synth !== 'undefined' && synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = populateVoiceList;
+  }
+
+  talkButton.addEventListener('click', () => {
+    const utterThis = new SpeechSynthesisUtterance(textarea.value);
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+
+    for (const voice of synth.getVoices()) {
+      if (voice.name === selectedOption) {
+        utterThis.voice = voice;
+      }
+    }
+
+    imgElement.src = openMouth;
+    synth.speak(utterThis);
+    utterThis.onend = () => {
+      imgElement.src = closedMouth;
+    };
+  });
+}
